7e4349507c87ba01e0584f0fd50b349c7fa4684d
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-10-12 19:48:10 +0100

    add ninjas module

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+
+@Controller('ninjas')
+export class NinjasController {
+  // GET /ninjas --> []
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  // GET /ninjas/id --> { ... }
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /ninjas
+  @Post()	
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  // PUT /ninjas/:id --> { ... }
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto ) {
+	  return {
+		  id,
+		  name: updateNinjaDto.name,
+	};
+  }
+
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return { id };
+  }
+}
