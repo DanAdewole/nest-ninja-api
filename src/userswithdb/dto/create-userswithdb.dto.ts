503aa6db700794cc9bcbf34f0c90abd150b0286b
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-10-15 23:12:11 +0100

    add data for the dto

--- src/userswithdb/dto/create-userswithdb.dto.ts
+++ src/userswithdb/dto/create-userswithdb.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserswithdbDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateUserswithdbDto {
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
